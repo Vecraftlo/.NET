221c89a17c70f0316cbb2e44da8a094ca1aa7fd9
unknown <dawid.fydryszewski@gmail.com>
2022-11-12 11:08:16 +0100

    first commit in the repo

--- zadania/TypyDanych2/Program.cs
+++ zadania/TypyDanych2/Program.cs
@@ -8,14 +8,47 @@ for (int number = 1; number <= silnia; number++)
 Console.WriteLine(ndwa);
 
 
-int silnia2 = 1;
-int n = 12;
-int cn = n;
+// int silnia2 = 1;
+// int n = 12;
+// int cn = n;
 
-while(n != 0)
-{
-    silnia2 *= n;
-    n --;
-}
+// while (n != 0)
+// {
+//     silnia2 *= n;
+//     n--;
+// }
 
-Console.WriteLine(cn + "! = " + silnia2);
+// Console.WriteLine(cn + "! = " + silnia2);
+
+//ZADANIE 3 Wyświetl trzeci bok trójkąta prostokątnego dla podanych dwóch jego boków przyprostokątnych.
+// Wybierz dowolne liczby.
+
+//WZÓR
+// int a = 4;
+// int b = 3;
+// int c = (a*a)+(b*b);
+// Console.WriteLine("Przeciwprostokątna jest równa:" +c);
+
+//DZIAŁA, ALE JEST INFINITE LOOP
+// int a = 4;
+// int b = 3;
+// for(int c =1; c >0; c++)
+// {
+//     c*c = (a*a)+(b*b);
+// Console.WriteLine("Przeciwprostokątna jest równa:" +c);
+// }
+
+// int a = 4;
+// int b = 3;
+// for (int c =1; c<100; c++)
+// {
+//     if (c!=0)
+//     {
+//     c*c=(a*a)+(b*b);
+//     Console.WriteLine("Przeciwprostokątna ma wartość:"+c);
+//     }
+//     else
+//     {
+//         return;
+//     }
+// }
