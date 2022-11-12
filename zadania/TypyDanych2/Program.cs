08da169f6a1a6e5f337a46737aaac8d9b70db2f4
unknown <dawid.fydryszewski@gmail.com>
2022-11-12 10:59:53 +0100

    first commit in the repo

--- zadania/TypyDanych2/Program.cs
+++ zadania/TypyDanych2/Program.cs
@@ -1,20 +1,21 @@
 ﻿
 int silnia = 12; //bo silnia z 12 - n!=1⋅2⋅3⋅...⋅(n−1)⋅n
-int ndwa = 1;
+int ndwa = 1; //
 for (int number = 1; number <= silnia; number++)
 {
     ndwa = ndwa * number;
 }
 Console.WriteLine(ndwa);
 
-// int silnia (int n)
-// {
-//     if (n>1) 
-//     { 
-//       return silnia(n-1)*n; 
-//     }
-//     else 
-//     { 
-//       return 1; 
-//     }
-// }
+
+int silnia2 = 1;
+int n = 12;
+int cn = n;
+
+while(n != 0)
+{
+    silnia2 *= n;
+    n --;
+}
+
+Console.WriteLine(cn + "! = " + silnia2);
--- zadania/TypyDanych3/Program.cs
+++ zadania/TypyDanych3/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+int x = 3;
+int result = -1 * (x * x * x) + 2 * (x * x) - 1 * x - 4;
+Console.WriteLine("Zadanie 1: " + result);
+
+int result2 = 9 * (x * x * x * x) - 12 * (x * x) + 4;
+Console.WriteLine("Zadanie 2: " + result2);
+
+int result3 = 9 * (x * x * x * x) + 12 * (x * x) + 4;
+Console.WriteLine("Zadanie 3: " + result3);
+
+int result4 = 9 * (x * x * x * x) - 4;
+Console.WriteLine("Zadanie 4: " + result4);
+
+int result5 = 9 * (x * x * x * x) + 4;
+Console.WriteLine("Zadanie 5: " + result5);
+
+int result6 = x * x - 4 * x + 4;
+Console.WriteLine("Zadanie 6: " + result6);
+
+int result7 = x * x + 2 * x + 4;
+Console.WriteLine("Zadanie 7: " + result7);
+
+int result8 = (1 - x) * (x + 2) * ((x * x) - 4);
+Console.WriteLine("Zadanie 8: " + result8);
+
+int result9 = (x - 3) * (x + 3) * ((x * x) - 9);
+Console.WriteLine("Zadanie 9: " + result9);
+
+int result10 = ((x * x) + 1) + ((x * x) + 4) * ((x * x) + 3);
+Console.WriteLine("Zadanie 10: " + result10);
+
+int result11 = (2 - x) * (x + 3)*((x * x) - 1);
+Console.WriteLine("Zadanie 11: " + result11);
+
+int result12 = x * x * x - 3 * (x * x) + 5 * x - 15;
+Console.WriteLine("Zadanie 12:" + result12);
+
+int result13 = 2 * (x * x * x) - (x * x) - 6 * x + 3;
+Console.WriteLine("Zadanie 13: " + result13);
+
